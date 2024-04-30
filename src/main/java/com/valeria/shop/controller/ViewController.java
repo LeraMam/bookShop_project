3f7ca7df78a5260ceebf036a33b158b7291cfe20
Valeria <valeria.mamaiko@gmail.com>
2024-04-30 13:27:48 +0300

    docker file added

--- src/main/java/com/valeria/shop/controller/ViewController.java
+++ src/main/java/com/valeria/shop/controller/ViewController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
